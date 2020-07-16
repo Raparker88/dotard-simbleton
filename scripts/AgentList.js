d07075475ff56512a226934dad2a1542b00ef775
Raparker88 <raparker88@gmail.com>
2020-07-16 18:29:31 -0500

    added search elements for businesses and agents

--- scripts/AgentList.js
+++ scripts/AgentList.js
@@ -11,3 +11,34 @@ export const agentList = () => {
         contentElement.innerHTML += agentHTML(agent)
     })
 }
+const companySearchResultArticle = document.querySelector(".foundAgents")
+
+document
+    .querySelector("#agentSearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+            /*
+                When user presses enter, find the matching business.
+                You can use the `.includes()` method strings to
+                see if a smaller string is part of a larger string.
+
+                Example:
+                    business.companyName.includes(keyPressEvent.target.value)
+            */
+            const agents = agentCollection()
+            const foundAgents = agents.find(agent => agent.fullName.includes(keyPressEvent.target.value))// implement .find() method here
+
+            companySearchResultArticle.innerHTML = `
+                <h2>
+                ${foundAgents.fullName}
+                </h2>
+                <section>
+                ${foundAgents.company}
+
+                </section>
+                <section>
+                ${foundAgents.phoneNumber}
+                </section>
+            `;
+        }
+    });
--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,3 @@ businessList()
 import { agentList } from "./AgentList.js"
 agentList()
 
-import { agentCollection } from "./BusinessProvider.js"
-console.log(agentCollection())
